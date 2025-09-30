82e534c4892b568f5892df726c957808dfddb055
popeye105 <arbabrizvi17@gmail.com>
2025-10-01 02:07:11 +0530

    alrightfinal

--- js/main.js
+++ js/main.js
@@ -228,7 +228,7 @@ function initMobileNavigation() {
                     if (targetId === 'about') {
                         offset = -50; 
                     } else if (targetId === 'projects' || targetId === 'certifications') {
-                        offset = -20; 
+                        offset = -10; 
                     }
                     console.log(`MOBILE NAV FIRED: ${targetId}, offset: ${offset}`); 
                     const offsetTop = targetSection.offsetTop - offset;
